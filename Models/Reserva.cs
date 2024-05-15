52d44ffa236d9bd3c4b4cd754f91d283569559b8
lucasSax <lucas.aut166@gmail.comgit config --global user.email lucas.aut166@gmail.com>
2024-05-15 15:25:05 -0300

    Desafio concluído

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,16 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // IMPLEMENTADO
+
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                 // IMPLEMENTADO
+                throw new Exception (" Ocorreu um erro! O número de hospedes é maior que a capacidade da suíte. ");
             }
         }
 
@@ -34,24 +34,22 @@ namespace DesafioProjetoHospedagem.Models
         }
 
         public int ObterQuantidadeHospedes()
-        {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+        { 
+            // IMPLEMENTADO
+            int qtdHospedes = Hospedes.Count();
+            return qtdHospedes;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+             // IMPLEMENTADO
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+           
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+             // IMPLEMENTADO
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor -= valor * 0.1m; 
             }
 
             return valor;
